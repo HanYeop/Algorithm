6e8b84674304b279e0c94e4375d347fab8843e03
Sangyeop Han <50766393+HanYeop@users.noreply.github.com>
2021-04-28 13:16:40 +0900

    [백준] 10828. 스택 (Kotlin)

---
+++ 10828.kt
@@ -0,0 +1,28 @@
+fun main() = with(System.`in`.bufferedReader()){
+    val stack = arrayListOf<Int>()
+    val n = readLine().toInt()
+
+    for(i in 0 until n){
+        val tmp = readLine().split(" ")
+        when(tmp[0]){
+            "push" -> {
+                stack.add(tmp[1].toInt())
+            }
+            "pop" ->{
+                if (stack.isNotEmpty()) println(stack.removeAt(stack.size - 1))
+                else println("-1")
+            }
+            "size" ->{
+                println(stack.size)
+            }
+            "empty" ->{
+                if (stack.isEmpty()) println("1")
+                else println("0")
+            }
+            "top" -> {
+                if (stack.isNotEmpty()) println(stack[stack.size - 1])
+                else println("-1")
+            }
+        }
+    }
+}
