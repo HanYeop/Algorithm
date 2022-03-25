2086155dfd927204dee2444e54e9fa5b9f0e3ae0
Han Sangyeop <50766393+HanYeop@users.noreply.github.com>
2022-03-25 19:04:00 +0900

    [백준] 1002. 터렛 (Kotlin)

---
+++ 1002.kt
@@ -0,0 +1,19 @@
+// [백준] 1002. 터렛 (Kotlin)
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    val n = readLine().toInt()
+
+    for(i in 0 until n){
+        val st = StringTokenizer(readLine())
+        val x1 = st.nextToken().toInt()
+        val y1 = st.nextToken().toInt()
+        val r1 = st.nextToken().toInt()
+        val x2 = st.nextToken().toInt()
+        val y2 = st.nextToken().toInt()
+        val r2 = st.nextToken().toInt()
+    }
+
+}
